36295ab1e916d9815f66283ee6e16f4a421a4bc6
jameswagner <jamesrwagner@gmail.com>
2024-05-27 15:24:09 -0500

    try starting first

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ from typing import List
 import random
 import string
 
-import hiredis
-
 class AsyncServer:
     def __init__(self, host: str = "127.0.0.1", port: int = 6379, replica_server: str = None, replica_port: int = None):
         self.host = host
@@ -111,7 +109,6 @@ class AsyncRequestHandler:
 
     async def handle_request(self, request: bytes) -> None:
         command = self.parse_redis_protocol(request)
-        print(f"COMMAND: {command}")
         if not command:
             logging.info("Received invalid data")
             return
@@ -211,22 +208,27 @@ class AsyncRequestHandler:
         
         return ''.join(encoded_data).encode()
 
-
     def parse_redis_protocol(self, data: bytes):
-        parser = hiredis.Reader()
-        parser.feed(data)
-        
-        commands = []
-        while True:
-            try:
-                command = parser.gets()
-                if command is False:
-                    break
-                commands.append(command)
-            except hiredis.ProtocolError:
+        try:
+            parts = data.split(b'\r\n')
+            if not parts or parts[0][0] != ord(b'*'):
                 return None
+            
+            num_elements = int(parts[0][1:])
+            elements = []
+            index = 1
+            
+            for _ in range(num_elements):
+                if parts[index][0] != ord(b'$'):
+                    return None
+                index += 1
+                elements.append(parts[index].decode('utf-8'))
+                index += 1
+            print("ELEMENTS: ", elements)
 
-        return commands
+            return elements
+        except (IndexError, ValueError):
+            return None
         
     
         
