4a5079ed6bb228f706193990b8c82071730d390c
jameswagner <jamesrwagner@gmail.com>
2024-06-06 12:28:34 -0500

    add indefinitely blocking xread

--- app/main.py
+++ app/main.py
@@ -355,7 +355,7 @@ class AsyncRequestHandler:
                     stream_ids = [x for x in command[command.index(next(filter(lambda x: re.match(r'\d+-\d+', x), command))):] if re.match(r'\d+-\d+', x)]
                     for stream_key, stream_id in zip(stream_keys, stream_ids):
                         response = self.get_one_xread_response(stream_key, stream_id)
-                        if response != "$1-1\r\n":
+                        if response != "$-1\r\n":
                             found = True
                             break
                     await asyncio.sleep(0.05)
