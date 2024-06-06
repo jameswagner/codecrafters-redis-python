31776d3a294a4dd4c5bcb7ee5325243cdab5eed4
jameswagner <jamesrwagner@gmail.com>
2024-06-06 13:54:27 -0500

    add $ for xread

--- app/main.py
+++ app/main.py
@@ -350,13 +350,12 @@ class AsyncRequestHandler:
                 await asyncio.sleep(block_time / 1000)
                 if command[len(command) - 1] == "$":
                     stream_keys = command[start_index:command.index(next(filter(lambda x: re.match(r'\$', x), command)))]
-                    stream_ids = [self.server.streamstore[stream_key][len(self.server.streamstore[stream_key].keys())-1] for stream_key in stream_keys]               
+                    stream_ids = [self.server.streamstore[stream_key].keys()[-1] for stream_key in stream_keys]               
             else:
                 found = False
                 if command[len(command) - 1] == "$":
                     stream_keys = command[start_index:command.index(next(filter(lambda x: re.match(r'\$', x), command)))]
-                    print(f"Stream keys: {stream_keys}")
-                    stream_ids = [self.server.streamstore[stream_key][len(self.server.streamstore[stream_key].keys())-1] for stream_key in stream_keys]
+                    stream_ids = [self.server.streamstore[stream_key].keys()[-1] for stream_key in stream_keys]
                 while not found:
                     stream_keys = stream_keys or command[start_index:command.index(next(filter(lambda x: re.match(r'\d+-\d+', x), command)))]
                     stream_ids = stream_ids or [x for x in command[command.index(next(filter(lambda x: re.match(r'\d+-\d+', x), command))):] if re.match(r'\d+-\d+', x)]
